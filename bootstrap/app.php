902ec84e41272a3a4b8a0d753642d0dfbe266dad
Flavio Moreira <flavio.moreira@outlook.com>
2026-03-27 18:20:31 -0300

    .

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Inertia\Inertia;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -31,7 +30,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->respond(function (Response $response, Throwable $e, Request $request) {
+        $exceptions->respond(function ($response, Throwable $e, Request $request) {
 
             $status = $response->getStatusCode();
 
